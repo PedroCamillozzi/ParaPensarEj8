80ce87acedd9fa27238a1f73e95db14fc7dbdc0d
PedroCamillozzi <130605829+PedroCamillozzi@users.noreply.github.com>
2023-05-03 00:13:05 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaPensarEj8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 0;
+            string clave, claveCorrecta= "pepe123";
+            while (intentos < 4)
+            {
+                Console.WriteLine("Ingrese la clave");
+                clave = Console.ReadLine();
+                if (clave == claveCorrecta)
+                {
+                    Console.WriteLine("Bienvenido");
+                    Console.ReadKey();
+                    break;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Clave Fallida");
+                    intentos++;
+                }
+            }
+        }
+    }
+}
